59f4c8c2b1d94f4057b05154fd8efacd9b75252a
Supriy-Swain08 <supriya.swain2018@gmail.com>
2023-03-12 21:32:56 +0530

    This is my Third Commit

--- app/src/main/java/com/example/bikenest/MainActivity.kt
+++ app/src/main/java/com/example/bikenest/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.bikenest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNav:BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNav=findViewById(R.id.bottomNavigationView)
+
+        val Home=findViewById<FloatingActionButton>(R.id.Home)
+        Home.setOnClickListener{
+            loadFragment(HomeFragment())
+        }
+
+        loadFragment(HomeFragment())
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.Profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                R.id.Wallet -> {
+                    loadFragment(WalletFragment())
+                    true
+                }
+                R.id.Favourite -> {
+                    loadFragment(FavFragment())
+                    true
+                }
+
+                else -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+            }
+        }
+
+
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
--- app/src/main/java/com/example/bikenest/Sign_up.kt
+++ app/src/main/java/com/example/bikenest/Sign_up.kt
@@ -3,45 +3,90 @@ package com.example.bikenest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.auth.User
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Sign_up : AppCompatActivity() {
-    private lateinit var  sendOTPBtn:AppCompatButton
-    private lateinit var  personName:TextInputEditText
-    private lateinit var  EmailET:TextInputEditText
-    private lateinit var  UserPasswordForSignup:TextInputEditText
-    private lateinit var UserPasswordForSignupConfirm:TextInputEditText
+    private lateinit var sendOTPBtn: AppCompatButton
+    private lateinit var personName: TextInputEditText
+    private lateinit var EmailET: TextInputEditText
+    private lateinit var UserPasswordForSignup: TextInputEditText
+    private lateinit var UserPasswordForSignupConfirm: TextInputEditText
+
+    //////////////////////////////////////////////
+    private lateinit var auth: FirebaseAuth 
+    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        sendOTPBtn=findViewById(R.id.sendOTPBtn)
-        personName=findViewById(R.id.personName)
-        EmailET=findViewById(R.id.EmailET)
-        UserPasswordForSignup=findViewById(R.id.UserPasswordForSignup)
-        UserPasswordForSignupConfirm=findViewById(R.id.UserPasswordForSignupConfirm)
+        sendOTPBtn = findViewById(R.id.sendOTPBtn)
+        personName = findViewById(R.id.personName)
+        EmailET = findViewById(R.id.EmailET)
+        UserPasswordForSignup = findViewById(R.id.UserPasswordForSignup)
+        UserPasswordForSignupConfirm = findViewById(R.id.UserPasswordForSignupConfirm)
+        auth=FirebaseAuth.getInstance()
+
 
         sendOTPBtn.setOnClickListener {
-
+            val currentUser = auth.currentUser
             if (personName.text.toString().isEmpty()) {
                 personName.setError("Please Enter Your Name")
                 personName.requestFocus()
             } else if (EmailET.text.toString().isEmpty()) {
-                EmailET.setError("Enter your Number")
+                EmailET.setError("Enter your Email")
                 EmailET.requestFocus()
             } else if (UserPasswordForSignup.text.toString().isEmpty()) {
                 UserPasswordForSignup.setError("Enter Your Password")
                 UserPasswordForSignup.requestFocus()
-            } else if (UserPasswordForSignup.text.toString().equals(UserPasswordForSignupConfirm.text.toString())) {
-                startActivity(Intent(this, Otp::class.java))
-            } else {
-                UserPasswordForSignup.setError("Mismatched the Passwords")
+            } else if (!UserPasswordForSignup.text.toString()
+                    .equals(UserPasswordForSignupConfirm.text.toString())
+            ) {
                 UserPasswordForSignup.requestFocus()
-                UserPasswordForSignupConfirm.setError("Mismatched")
+                UserPasswordForSignupConfirm.setError("Password not matched")
                 UserPasswordForSignupConfirm.requestFocus()
+            } else if (currentUser != null) {
+                Toast.makeText(this, "User Already Exists", Toast.LENGTH_SHORT).show()
+            }else{
+                var email=EmailET.text.toString().trim()
+                var password=UserPasswordForSignup.text.toString().trim()
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(
+                        OnCompleteListener <AuthResult>{
+                                task->
+                            //stopProgress()
+                            if(task.isSuccessful){
+                                val firebaseUser: FirebaseUser =task.result!!.user!!
+                                FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+
+
+                                Toast.makeText(this, "${auth.uid}", Toast.LENGTH_SHORT).show()
+                            }
+                            else
+                            {
+                                Toast.makeText(this,task.exception!!.message.toString(),Toast.LENGTH_SHORT).show()
+                                
+                            }
+                        }
+                    )
+            ///////////////////////////////////////////////////////////////////
             }
         }
+
+
+
+
 
     }
 }
